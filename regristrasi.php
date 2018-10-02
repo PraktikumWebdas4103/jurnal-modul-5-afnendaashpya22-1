c7f66a1e669d6e7ef5ba3495526deb9e75241a39
afnendaashpya22 <43516021+afnendaashpya22@users.noreply.github.com>
2018-10-02 16:53:03 +0700

    Update regristrasi.php
    registrasi dan prosesregis digabung.

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,38 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
+<form action="" method="POST">
+	<center>
+		<h1>PENDAFTARAN</h1>
+		<table>
+			<tr><td>NIM :<td></tr>
+			<tr><td><input type="text" name="nim" placeholder="nim" required></td></tr>
 
+			<tr><td>Nama :</td></tr>
+			<tr><td><input type="text" name="nama" placeholder="nama" required></td></tr>
 
+			<tr><td>Email :</td></tr>
+			<tr><td><input type="email" name="email" placeholder="@gmail.com" required=""></td></tr>
 
-</body>
-</html>
+			<tr><td><input type="submit" name="submit" value="Kirim"></td></tr>
+		</table>
+	</center>
+</form>
+
+<?php
+	if (isset($_POST['submit'])) {
+		$nim = $_POST['nim'];
+		$nama = $_POST['nama'];
+		$email = $_POST['email'];
+		
+		include "conn.php";
+
+			if (strlen($_POST['nim']) != 10 && strlen($_POST['nama']) >= 25) {
+				echo "Nim harus 10 karakter atau nama terlalu panjang!!!";
+			}
+			elseif(strlen($_POST['nim']) != 10 && strlen($_POST['nama']) <= 25){
+				$sql = mysqli_query($conn, "INSERT INTO `mahasiswa`(`nim`, `nama`, `email`) VALUES ('$nim', '$nama', '$email')");
+				if ($sql) {
+					$_SESSION['nim']=$nim;
+					echo "BERHASIL LOGIN";
+				}
+			}
+	}
+?>
