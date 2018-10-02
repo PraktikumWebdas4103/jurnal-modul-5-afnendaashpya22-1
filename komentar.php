986c8e2f0f0159b76ccb979acc451688527af703
afnendaashpya22 <43516021+afnendaashpya22@users.noreply.github.com>
2018-10-02 16:53:33 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,3 +1,29 @@
-<?php
+<form action="" method="POST">
+	<center>
+		<h1>KOMENTAR</h1>
+		<table>
+			<tr>
+				<td>Isi komentar :</td>
+			</tr>
+			<tr>
+				<td><input type="text" name="komen" placeholder="Komentar Anda"></td>
+			</tr>
+			<tr>
+				<td><input type="submit" name="submit" value="Posting"></td>
+			</tr>
+		</table>
+	</center>
+</form>
 
+<?php
+	if (isset($_POST['submit'])) {
+		if (str_word_count($_POST['komen']) < 5) {
+			echo "<center><b>Pesan error!!!</b></center>";
+		}
+		else{
+			$kata = str_word_count($_POST['komen']);
+			echo "<center><b>Jumlah kata pada kolom komentar : $kata </b></center>";
+		}
+	}
+	
 ?>
